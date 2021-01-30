14a3af0c5596c36cf9c7897bedc64ffd98ffd22e
Marat Magomedov <m.magomedov@telphin.ru>
2021-01-31 02:07:25 +0300

    переделал главную

--- src/index.js
+++ src/index.js
@@ -23,15 +23,19 @@ $(document).ready(function () {
   });
 
   // выбор решения
-  $('#firstBlock > .radio__wrap > .radio').click(function () {
-    $('#secondBlock').slideDown(400);
+  $('#firstBlock > .main__product > .radio').click(function () {
+    $('#secondBlock').css('display', 'grid');
   });
 
-  $('#secondBlock > .radio__wrap > .radio').click(function () {
+  $('#secondBlock > .main__product-wrap > .main__product > .radio').click(function () {
     $('#thirdBlock').css('display', 'flex');
   });
 
   $('#openOrderPopup').click(function () {
+    $('#orderPopup').addClass('active');
+  });
+
+  $('#openOrderPopup2').click(function () {
     $('#orderPopup').addClass('active');
   });
 
@@ -42,7 +46,13 @@ $(document).ready(function () {
   $('#orderSubmit').click(function () {
     $('#orderPopup').removeClass('active');
     $('#orderAccess').addClass('active');
-    // setTimeout($('#orderAccess').removeClass('active'), 3000);
+  });
+
+  // выбор радио
+  let radio = $('.radio');
+  radio.click(function () {
+    $('input:radio').parent('.main__product').css('background', '#fff');
+    $('input:radio:checked').parent('.main__product').css('background', '#27b7bf');
   });
 });
 
